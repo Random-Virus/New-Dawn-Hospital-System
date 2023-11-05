3093f86e01f9b8d1b47b4c1d62076f8ad1048b5a
Random-Virus <itumelengolebogeng8@gmail.com>
2023-11-05 23:40:43 +0200

    sdfsdf

--- selfqueue/models.py
+++ selfqueue/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from appointment.models import Doctor
 from django.contrib.auth.models import User  # Im
+class Feedback(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    survey_question = models.CharField(max_length=3, choices=[('yes', 'Yes'), ('no', 'No')])
+    rating = models.PositiveIntegerField()
+    message = models.TextField()
+    
 class Symptom(models.Model):
     name = models.CharField(max_length=255)
 
@@ -26,3 +33,16 @@ class PatientQueue(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+class MedicalRecord(models.Model):
+    patient = models.ForeignKey(PatientQueue, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='medical_records')
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    diagnosis = models.TextField()
+    prescription = models.TextField()
+    service = models.CharField(max_length=100)
+    date = models.DateField()
+    referrals = models.TextField(blank=True)
+    further_examination = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Medical Record for {self.patient.first_name} {self.patient.last_name}"
--- selfqueue/urls.py
+++ selfqueue/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
       path('', views.results, name='results'),
       path('feedback/', views.feedback, name='feedback'),# For the main page
       path('view_queue/', views.view_queue, name='view_queue'),
+      path('selfqueue/feedback/<int:doctor_id>/', views.feedback, name='feedback'),
       path('writeReport/', views.writeReport, name='writeReport'),
-      path('take-in/<int:id_number>/', views.take_in_patient, name='take_in_patient'),
-      path('writeReport/<int:id_number>/', views.writeReport, name='writeReport'),
+      path('take-in/<int:spot_number>/', views.take_in_patient, name='take_in_patient'),
+      path('writeReport/<int:spot_number>/', views.writeReport, name='writeReport'),
 ]
